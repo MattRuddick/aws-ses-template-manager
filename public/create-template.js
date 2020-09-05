83a1d6cff9921774bd6613baa635cdd8c8987ac1
MattRuddick <mattrudd1392@hotmail.com>
2020-09-05 17:47:22 +0100

    Save new template form submission

--- public/create-template.js
+++ public/create-template.js
@@ -1,19 +1,22 @@
-function createTemplate() {
-  const createPayload = {
-    "TemplateName": $('#templateName').val(),
-    "HtmlPart": $('#templateHtml').val(),
-    "SubjectPart": $('#templateSubject').val(),
-    "TextPart": $('#templateText').val()
-  };
+$(document).ready(function(){
+  $('#createTemplateForm').submit(function(e) {
+    e.preventDefault();
+    const createPayload = {
+      "TemplateName": $('#templateName').val(),
+      "HtmlPart": $('#templateHtml').val(),
+      "SubjectPart": $('#templateSubject').val(),
+      "TextPart": $('#templateText').val()
+    };
 
-  $.ajax({
-    type: "POST",
-    url: "/create-template",
-    data: createPayload,
-    success: (response) => {
-      window.location.href = '/';
-    }
+    $.ajax({
+      type: "POST",
+      url: "/create-template",
+      data: createPayload,
+      dataType: 'application/json',
+      success: function() {
+        window.location.href = '/';
+      }
+    });
   });
+});
 
-  //todo-> error checking
-}
